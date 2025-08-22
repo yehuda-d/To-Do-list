74bf8ca231e6e92166c4b2eb276a8c516910d17e
yehuda-d <yehuda130@gmail.com>
2025-08-22 10:28:17 +0300

    added array list

--- ToDoList.js
+++ ToDoList.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let list = [];
+list = ["shopping","sport","homework"];
 app.use(express.json());
 
 app.get('/',(req,res)=>{res.sendFile(__dirname + '/ToDoList.html')});
+
+app.get('/list',(req,res)=>{
+    res.json(list)
+});
+
+
+
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
