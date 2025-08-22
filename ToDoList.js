1656a9845e72874399a735f90831c1f89034477b
yehuda-d <yehuda130@gmail.com>
2025-08-22 12:12:50 +0300

    create tasks and validation

--- ToDoList.js
+++ ToDoList.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-let list = [];
-list = ["shopping","sport","homework"];
+
+const tasks = [];
+let nextID = 1;
+
 app.use(express.json());
 
 app.get('/',(req,res)=>{res.sendFile(__dirname + '/ToDoList.html')});
 
-app.get('/list',(req,res)=>{
-    res.json(list)
+app.get('/tasks',(req,res)=>{
+    res.status(200).json(tasks)
 });
+
+app.post('/tasks',(req,res)=>{
+    let text = req.body.txt;
+    if(!text){
+        return res.status(400).json({message:"not valid"})
+       
+    }
+    
+    let id = nextID++;
+    let task = {id,text};
+    tasks[id] = task;
+     res.status(201).json({message:"ok"})
+
+})
 
 
 
