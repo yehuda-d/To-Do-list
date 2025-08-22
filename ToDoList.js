1d07fce184b8ba3137ca123c1e4f99d18341954f
yehuda-d <yehuda130@gmail.com>
2025-08-22 12:42:56 +0300

    added delete of CRUD

--- ToDoList.js
+++ ToDoList.js
@@ -27,6 +27,15 @@ app.post('/tasks',(req,res)=>{
 
 })
 
+app.delete('/tasks/:id',(req,res)=>{
+    let id = req.params.id;
+    if(id < 0 || tasks.length < id){
+        return res.status(400).json({message:"אינו קיים"})
+    }
+    tasks[id] = null;
+    res.status(201).json({message:"Deleted"})
+});
+
 
 
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
